7ae84e480c15033eaa1d4c3af3ce91a6e735b8b8
chenshitao <452306233@qq.com>
2017-08-08 20:19:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
 import urllib.request as ur
-url_1="http://list.youku.com/category/show/c_97_g_"
-key_1="古装"
-key_1_b=ur.quote(key_1)
-key_2="大陆"
-key_2_b=ur.quote(key_2)
-url_2="_r_2016_pt_0_s_1_d_1.html?spm=a2h1n.8251845.filterPanel.5!6~1~3~A"
-url=url_1+key_1_b+"_a_"+key_2_b+url_2
+import re
+class return_url:
+    def __init__(self, type, place, year):
+        self.type = type
+        self.place = place
+        self.year = year
+    def url(self,yema):
+        url_1 = "http://list.youku.com/category/show/c_97_g_"
+        key_1 = self.type
+        key_1_b = ur.quote(key_1)
+        key_2 = self.place
+        key_2_b = ur.quote(key_2)
+        url_2 = "_r_"
+        key_year = str(self.year)
+        url_2_1 = "_pt_0_s_1_d_1_p_"
+        key_3 = str(yema)
+        url_3 = ".html?spm=a2h1n.8251845.filterPanel.5!3~1~3!2~A"
+        self.url = url_1 + key_1_b + "_a_" + key_2_b + url_2 + key_year + url_2_1 + key_3 + url_3
+        return self.url
+
+
+class youku:
+    def __init__(self,url):
+        self.url_=url
+
+    def all_name(self):
+        regex_mz = re.compile(rb'<a href="//list.*?title="(?P<mz>.{,100})" target="_blank">(?P=mz)</a>', re.S)
+        bb=bytes("下一页",encoding="utf-8")
+        regex_yeshu=re.compile(rb'<a href=.*?charset="-4-1-999"'+bb+rb'</a>')
+        data = ur.urlopen(self.url_.url(1))
+        file = data.read()
+        l=regex_mz.findall(file)
+        y=2
+        while regex_yeshu.search(file):
+            url_dz=self.url_.url(y)
+            file = ur.urlopen(url_dz).read()
+            list_ = regex_mz.findall(file)
+            l+list_
+            y=y+1
+        list_mz = [str(i, encoding="utf-8") for i in l]
+        return list_mz
+
+url=return_url("古装","大陆",2016)
+#yk=youku(url)
+#li=yk.all_name()
+print(url.url(1))
